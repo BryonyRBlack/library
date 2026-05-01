2de76abcccaa6019c5fb7ad8a5dffecc0cf86307
BryonyRBlack <ikow8b1w@students.codeinstitute.net>
2026-05-01 17:07:09 +0100

    Add testing

---
+++ src/test/java/com/nology/library/Users/UserTest.java
@@ -0,0 +1,18 @@
+package com.nology.library.Users;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+    @Test
+    void returnUserName() {
+        User user = new User(7, "Jenny", "password");
+        assertEquals("Jenny", user.getName());
+    }
+
+    @Test
+    void inputPassword() {
+    }
+}
